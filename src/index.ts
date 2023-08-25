56df719a20fafeb6cc77d85652ab38a25f6ca235
Outdam Meas <mstdam@gmail.com>
2023-08-26 06:26:19 +1000

    Add Express and Type

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
